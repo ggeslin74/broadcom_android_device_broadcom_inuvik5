50968a1419286f37d0074f9692cfaf65eae3047b
Pierre Couillaud <pierre.couillaud@broadcom.com>
2021-01-28 17:21:56 -0800

    inuvik5: allow to build for an extended device profile
    the device extended profile allows to alter the base android
    device profile for a particular target by massaging the device
    configuration.
    
    refs #SWANDROID-9387
    
    Change-Id: I52decb53074d5052396e8b06aaac5a5b877c4390
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- inuvik5.mk
+++ inuvik5.mk
@@ -2,6 +2,13 @@
 ifndef LOCAL_PRODUCT_OUT
 LOCAL_PRODUCT_OUT         := inuvik5
 endif
+
+LOCAL_SUBVAR_NRDP_STRICT ?= n
+ifneq (${WITH_SUBVAR},)
+#$(warning "enabling extension '${WITH_SUBVAR}'")
+include device/broadcom/inuvik5/subvar/${WITH_SUBVAR}.mk
+endif
+
 BCHP_CHIP                 := 72165
 BCHP_VER                  := A0
 NEXUS_PLATFORM            := 972165
@@ -10,7 +17,6 @@ LOCAL_DEVICE_BDIST_TARGET := inuvik5
 TARGET_SAGE_PLATFORM      := inuvik5
 LOCAL_DEVICE_SEPOLICY_IOCTL ?= nvk
 LOCAL_DEVICE_SEPOLICY_BLOCK_INSTANCE := device/broadcom/inuvik5/sepolicy/treble/${LOCAL_DEVICE_SEPOLICY_IOCTL}
-LOCAL_DEVICE_NRDP_OEMMODEL ?= google_inuvik5
 
 # use box mode 1 by default
 LOCAL_DEVICE_RTS_MODE        := 1
@@ -19,9 +25,16 @@ ANDROID_ENABLE_DHD_SECDMA    := n
 # wlan|bt gpio update
 LOCAL_DEVICE_AON_GPIO        := device/broadcom/inuvik-common/gpio/aon_gpio.nvk5.cfg:$(TARGET_COPY_OUT_VENDOR)/power/aon_gpio.cfg
 
-include device/broadcom/inuvik/inuvik.mk
+include device/broadcom/inuvik-common/inuvik.base.mk
 
 PRODUCT_NAME                   := inuvik5
 PRODUCT_MODEL                  := inuvik5
 PRODUCT_BRAND                  := google
 PRODUCT_DEVICE                 := inuvik5
+
+# Netflix support
+LOCAL_DEVICE_NRDP_MODEL_GROUP := INUVIK5
+LOCAL_DEVICE_NRDP_OEMMODEL ?= google_inuvik5
+
+TARGET_BOOTLOADER_BOARD_NAME  := inuvik5
+
